0c6f79dc0105269d8c04b82baceec0052908f4b0
Luke Childs <lukechilds123@gmail.com>
2018-05-27 10:03:14 +0700

    Add support for ETH testnet and mainnet

--- truffle.js
+++ truffle.js
@@ -1,3 +1,9 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+require('dotenv').config();
+const mnemonic = process.env.MNEMONIC;
+const token = process.env.INFURA_TOKEN;
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -6,6 +12,14 @@ module.exports = {
       host: '127.0.0.1',
       port: 8545,
       network_id: '*' // Match any network id
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${token}`),
+      network_id: 3
+    },
+    mainnet: {
+      provider: () => new HDWalletProvider(mnemonic, `https://mainnet.infura.io/${token}`),
+      network_id: 1
     }
   }
 };
