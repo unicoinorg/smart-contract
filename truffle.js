6d02337f3567fe46fb4f40c01afe2252b4060033
Luke Childs <lukechilds123@gmail.com>
2018-05-27 13:49:03 +0700

    Clean up truffle.js

--- truffle.js
+++ truffle.js
@@ -5,13 +5,11 @@ const mnemonic = process.env.MNEMONIC;
 const token = process.env.INFURA_TOKEN;
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
   networks: {
     development: {
       host: '127.0.0.1',
       port: 8545,
-      network_id: '*' // Match any network id
+      network_id: '*'
     },
     ropsten: {
       provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${token}`),
